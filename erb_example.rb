71bf3cbdade0d1c18bb5e413571cd7187b529ee8
kelemo99 <kelemo99@gmail.com>
2014-02-18 13:34:02 -0700

    First Homework Change
    Turned x into an array.
    Added function that counted number of numbers in array.
    Changed template variable to m.

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,7 @@
 require 'erb'
-
-x = 42
-template = ERB.new "The value of x is: <%= x %>"
-puts template.result(binding)
+x = ['2','3','99999999999999']
+y=x.count
+n = ERB.new "The value of x is: <%= x %>"
+puts n.result(binding)
+m = ERB.new "The number of numbers in this array is: <%= y %>"
+puts m.result(binding)
